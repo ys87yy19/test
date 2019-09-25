568a962e0247d5f9f35ad0033c2cdf7aa3dc8987
ye87yy19 <ys87yy19@gmail.com>
2019-09-26 07:38:13 +0800

    w sd

---
+++ yy19.c
@@ -0,0 +1,3 @@
+"
+解决git bash 终端显示中文乱码 要注意的是,这样设置后,你的git bash终端也要设置成中文和utf-8编码 才能正确显示中文,例如对比如下: 在git bash的界面中右击空白处,弹出菜单,选择选项->文本->本地Locale,设置为zh CN,而旁边的字符集选框选为UTF-8 英文显示则是: Options->Text->Locale改为zh CN,Character set改为UTF-8
+"
